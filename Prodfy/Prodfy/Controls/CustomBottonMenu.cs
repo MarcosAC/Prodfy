167518670a5368b1ff4d3f3ecbf711e14322d8ef
Marcos Aurelio <marcos.aucorrea@gmail.com>
2018-12-17 17:53:15 -0200

    Tabbed Page

--- Prodfy/Prodfy/Controls/CustomBottonMenu.cs
+++
@@ -1,8 +0,0 @@
-﻿using Xamarin.Forms;
-
-namespace Prodfy.Controls
-{
-    public class CustomBottonMenu : TabbedPage
-    {
-    }
-}
--- Prodfy/Prodfy/Views/BottomMenu.xaml.cs
+++ Prodfy/Prodfy/Views/BottomMenu.xaml.cs
@@ -1,17 +1,10 @@
-﻿using Prodfy.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Prodfy.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class BottomMenu : CustomBottonMenu
+    public partial class BottomMenu : TabbedPage
     {
         public BottomMenu()
         {
