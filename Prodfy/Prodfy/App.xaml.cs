942bb371bc3a230a7128041a9c187c305cdba7cd
Marcos Aurelio <marcos.aucorrea@gmail.com>
2018-12-21 11:41:04 -0200

    Pagina Funções.

--- Prodfy/Prodfy/App.xaml.cs
+++ Prodfy/Prodfy/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Prodfy
         {
             InitializeComponent();
 
-            MainPage = new SincronismoView();
+            MainPage = new FuncoesView();
         }
 
         protected override void OnStart()
