824915b7ebe88579d47390c8dd30ddd9af3e8d38
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-05 11:17:03 -0200

    Serviço de navegação entre paginas.

---
+++ Prodfy/Prodfy/Services/INavigationService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prodfy.Services
+{
+    public interface INavigationService
+    {
+        Task PushAsync(Page page);
+        Task PushModalAsync(Page page);
+        Task PushModalAsync();
+        Task PopAsync();
+    }
+}
---
+++ Prodfy/Prodfy/Services/NavigationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prodfy.Services
+{
+    public class NavigationService : INavigationService
+    {
+        public async Task PushAsync(Page page) => await App.Current.MainPage.Navigation.PushAsync(page);
+        public async Task PushModalAsync(Page page) => await App.Current.MainPage.Navigation.PushModalAsync(page);
+        public async Task PopAsync() => await App.Current.MainPage.Navigation.PopAsync();
+        public async Task PushModalAsync() => await App.Current.MainPage.Navigation.PopModalAsync();
+    }
+}
