f1c7c90be33f2d4090a0a5f254be3e45a2ddb225
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-08 16:58:26 -0200

    Effects

---
+++ Prodfy/Prodfy.Android/Effects/TextColorChangedEffect.cs
@@ -0,0 +1,41 @@
+﻿using Android.Widget;
+using Prodfy.Droid.Effects;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("MyEffects")]
+[assembly: ExportEffect(typeof(TextColorChangedEffect), "TextColorChangedEffect")]
+namespace Prodfy.Droid.Effects
+{
+    public class TextColorChangedEffect : PlatformEffect
+    {
+        //Android.Graphics.Color textColorChanged = Android.Graphics.Color.Gray;
+        Label control = new Label();
+
+        protected override void OnAttached()
+        {
+            //textColorChanged = Android.Graphics.Color.Gray;
+            control.TextColor = Color.Gray;
+        }
+
+        protected override void OnDetached()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);            
+
+            if (args.PropertyName == "IsTapped")
+            {
+                if (control.TextColor == Color.Gray)
+                {
+                    control.TextColor = Color.LightBlue;
+                }
+            }
+        }
+    }
+}
---
+++ Prodfy/Prodfy/Effects/TextColorChangedEffect.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace Prodfy.Effects
+{
+    public class TextColorChangedEffect : RoutingEffect
+    {
+        public TextColorChangedEffect() : base("MyEffects.TextColorChangedEffect")
+        {
+        }
+    }
+}
