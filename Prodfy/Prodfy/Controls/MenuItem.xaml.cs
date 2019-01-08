670f0fae0c5cae2e6c631d8f369a29b415869ee3
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-08 16:55:42 -0200

    Tentiva de Effects.

--- Prodfy/Prodfy/Controls/MenuItem.xaml.cs
+++ Prodfy/Prodfy/Controls/MenuItem.xaml.cs
@@ -1,20 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Prodfy.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Prodfy.Controls
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MenuItem : ContentView
 	{
 		public MenuItem ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void OnTapped_Inicio(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new LoginView());
+        }
+
+        private void OnTapped_Funcoes(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new FuncoesView());
+        }
+
+        private void OnTapped_Sincronia(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new SincronismoView());
+        }
+
+        private void OnTapped_Ajustes(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new AjustesView());
+        }
+    }
 }
