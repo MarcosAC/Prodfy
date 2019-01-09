494cd77fe135fd5f0bc6b7335c0400b45b53d3fe
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-09 16:27:18 -0200

    BaseViewModel.

--- Prodfy/Prodfy/ViewModels/BaseViewModel.cs
+++ Prodfy/Prodfy/ViewModels/BaseViewModel.cs
@@ -19,5 +19,17 @@ namespace Prodfy.ViewModels
             storege = value;
             OnPropertyChanged(propertyName);
         }
+
+        string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; OnPropertyChanged(); }
+        }
+
+        public BaseViewModel(string title)
+        {
+            Title = title;
+        }
     }
 }
