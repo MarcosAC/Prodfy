9c7b55c1c2a5b4fbeb4ea9cfd8a98a11735638b5
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-10 16:12:34 -0200

    Botões TitleView.

--- Prodfy/Prodfy/Controls/CustomTitleView.xaml.cs
+++ Prodfy/Prodfy/Controls/CustomTitleView.xaml.cs
@@ -36,27 +36,50 @@ namespace Prodfy.Controls
             titleView.titulo.Text = (string)newValue;
         }
 
-        public static readonly BindableProperty IconeProperty =
+        public static readonly BindableProperty Icone1Property =
            BindableProperty.Create(
-               propertyName: "Icone",
+               propertyName: "Icone1",
                returnType: typeof(string),
                declaringType: typeof(CustomTitleView),
                defaultValue: "",
                defaultBindingMode: BindingMode.TwoWay,
-               propertyChanged: IconePropertyChanged
+               propertyChanged: Icone1PropertyChanged
            );
 
-        public string Icone
+        public string Icone1
         {
-            get { return (string)GetValue(IconeProperty); }
-            set { SetValue(IconeProperty, value); }
+            get { return (string)GetValue(Icone1Property); }
+            set { SetValue(Icone1Property, value); }
         }
 
-        private static void IconePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        private static void Icone1PropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var titleView = (CustomTitleView)bindable;
 
-            titleView.icone.Text = (string)newValue;
+            titleView.icone1.Text = (string)newValue;
+        }
+
+        public static readonly BindableProperty Icone2Property =
+           BindableProperty.Create(
+               propertyName: "Icone2",
+               returnType: typeof(string),
+               declaringType: typeof(CustomTitleView),
+               defaultValue: "",
+               defaultBindingMode: BindingMode.TwoWay,
+               propertyChanged: Icone2PropertyChanged
+           );
+
+        public string Icone2
+        {
+            get { return (string)GetValue(Icone2Property); }
+            set { SetValue(Icone2Property, value); }
+        }
+
+        private static void Icone2PropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var titleView = (CustomTitleView)bindable;
+
+            titleView.icone2.Text = (string)newValue;
         }
     }
 }
