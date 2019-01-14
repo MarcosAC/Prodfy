ad70350427cb730fc31c9bf564897b42d97c26c8
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-14 09:05:07 -0200

    XF Atualizado.

---
+++ Prodfy/Prodfy/Models/Atividade.cs
@@ -0,0 +1,15 @@
+﻿namespace Prodfy.Models
+{
+    public class Atividade
+    {
+        public int idAtividade { get; set; }
+        public string disp_Id { get; set; }
+        public string colaborador_id { get; set; }
+        public string lista_atv_id { get; set; }
+        public string data_inicio { get; set; }
+        public string data_fim { get; set; }
+        public string obs { get; set; }
+        public string last_update { get; set; }
+        public int ind_sinc { get; set; }
+    }
+}
