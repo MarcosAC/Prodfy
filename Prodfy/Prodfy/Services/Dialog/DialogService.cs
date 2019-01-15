f311f4cdeaf366b214566997a7dd9e8ccc06e25e
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-15 09:53:04 -0200

    Serviço de mensagens e navegacao

---
+++ Prodfy/Prodfy/Services/Dialog/DialogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prodfy.Services.Dialog
+{
+    public class DialogService : IDialogService
+    {
+        public async Task AlertAsync(string title, string message, string cancel) 
+            => await App.Current.MainPage.DisplayAlert(title, message, cancel);
+
+        public async Task AlertAsync(string title, string message, string accept, string cancel) 
+            => await App.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+    }
+}
---
+++ Prodfy/Prodfy/Services/Dialog/IDialogService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Prodfy.Services.Dialog
+{
+    public interface IDialogService
+    {
+        Task AlertAsync(string title, string message, string cancel);
+        Task AlertAsync(string title, string message, string accept, string cancel);
+    }
+}
