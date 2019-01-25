02fc0752807f34d7cc81ea6d5a8b165af1a8921d
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-25 14:21:27 -0200

    Navegação pagina Ajustes.

--- Prodfy/Prodfy/Views/AjustesView.xaml.cs
+++ Prodfy/Prodfy/Views/AjustesView.xaml.cs
@@ -15,20 +15,5 @@ namespace Prodfy.Views
 
             BindingContext = new AjustesViewModel();
         }
-
-        private void OnClicked_Dispositivo(object sender, System.EventArgs e)
-        {
-            App.Current.MainPage.Navigation.PushAsync(new DispositivoView());
-        }
-
-        private void OnClicked_Conexao(object sender, System.EventArgs e)
-        {
-            App.Current.MainPage.Navigation.PushAsync(new ConexaoView());
-        }
-
-        private void OnClicked_Exportar(object sender, System.EventArgs e)
-        {
-            App.Current.MainPage.Navigation.PushAsync(new ExportarDadosView());
-        }
     }
 }
