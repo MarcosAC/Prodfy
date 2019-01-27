9cb37c50ef3d8486ebbecb50f575cc9188158ea3
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-01-27 18:10:39 -0200

    Classe com as constantes de acessoa a api.

---
+++ Prodfy/Prodfy/Helpers/Contantes.cs
@@ -0,0 +1,10 @@
+﻿namespace Prodfy.Helpers
+{
+    public class Contantes
+    {
+        public static string BASE_PROTOCOL = "http://";
+        public static string BASE_URL = "prodfy.com.br/plantas/";
+        public static string BASE_API = "app.php?";
+        public static string URL = BASE_PROTOCOL + BASE_URL + BASE_API;
+    }
+}
