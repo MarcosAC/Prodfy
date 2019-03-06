9cfbec7c5ff6d3bd192b1f69ed23709581d5d678
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-03-06 19:01:36 -0300

    Atualização.

---
+++ Prodfy/Prodfy/Utils/Login.cs
@@ -0,0 +1,30 @@
+﻿using Prodfy.Services.Repository;
+
+namespace Prodfy.Utils
+{
+    public static class Login
+    {
+        public static VerificaLogin VerificaLogin { get; }
+    }
+
+    public class VerificaLogin
+    {
+        private UserRepository userRepository;
+
+        public VerificaLogin()
+        {
+            userRepository = new UserRepository();
+        }
+
+        public bool UsuarioEstaLogado()
+        {
+            var dadosUser = userRepository.ObterDados();
+
+            if (dadosUser?.senha != null)
+                if (dadosUser?.senha == dadosUser?.senha)
+                    return true;
+
+            return false;
+        }
+    }
+}
