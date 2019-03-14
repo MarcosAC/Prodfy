b70dc93256a3ea163ba2ae2a077340cab759f953
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-03-14 19:20:46 -0300

    Custom Renderer desabilatar AllCaps.

---
+++ Prodfy/Prodfy.Android/Renderers/CustomButtonRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using Prodfy.Controls;
+using Prodfy.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
+namespace Prodfy.Droid.Renderers
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            var button = Control;
+            button.SetAllCaps(false);           
+        }
+    }
+}
