fee1ebfeebf0c12f83b199d541c032692f9b4b4f
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-03-30 14:40:46 -0300

    Atualização tabbedbottom.

--- Prodfy/Prodfy/App.xaml.cs
+++ Prodfy/Prodfy/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Prodfy
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new LoginView())
+            MainPage = new NavigationPage(new InicioView())
             {
                 BarBackgroundColor = Color.FromHex("#206805")
             };
---
+++ Prodfy/Prodfy/Views/InicioView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Prodfy.Controls;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace Prodfy.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InicioView : BottomNavTabPage
+    {
+        public InicioView ()
+        {
+            InitializeComponent();
+
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+    }
+}
