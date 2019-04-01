160f6edd2c3f6f7fb4ff2a29aceb1ebe7af930de
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-04-01 16:44:33 -0300

    Splash screen e icone do app.

--- Prodfy/Prodfy.Android/MainActivity.cs
+++ Prodfy/Prodfy.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace Prodfy.Droid
 {
-    [Activity(Label = "Prodfy", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Prodfy", Icon = "@drawable/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ Prodfy/Prodfy.Android/SplashScreen.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Prodfy.Droid
+{
+    [Activity(Label = "Prodfy Plantas", Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(typeof(MainActivity));        
+        }
+    }
+}
