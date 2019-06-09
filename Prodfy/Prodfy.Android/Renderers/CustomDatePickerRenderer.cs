ccba451924f350945dc3bf3f9e5695089dad6b7f
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-06-09 17:15:20 -0300

    Custom DatePicker ok

---
+++ Prodfy/Prodfy.Android/Renderers/CustomDatePickerRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Prodfy.Controls;
+using Prodfy.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomDatePicker), typeof(CustomDatePickerRenderer))]
+namespace Prodfy.Droid.Renderers
+{
+    public class CustomDatePickerRenderer : DatePickerRenderer
+    {
+        public CustomDatePickerRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+        }
+    }
+}
