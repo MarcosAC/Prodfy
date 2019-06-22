906ab899f92e50b6f25db4496d9b34dc73a366a7
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-06-22 15:40:32 -0300

    Datetime picker ok.

---
+++ Prodfy/Prodfy/Controls/CustomDateTimePicker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Prodfy.Controls
+{
+    public class CustomDateTimePicker : Entry, INotifyPropertyChanged
+    {
+        public DatePicker _datePicker { get; private set; } = new DatePicker() { MinimumDate = DateTime.Today, IsVisible = false };
+        public TimePicker _timePicker { get; private set; } = new TimePicker() { IsVisible = false };
+
+        string _stringFormat { get; set; }
+
+        public string StringFormat
+        {
+            get => _stringFormat ?? "dd/MM/yyyy HH:mm";
+            set => _stringFormat = value;
+        }
+
+        public DateTime DateTime
+        {
+            get => (DateTime)GetValue(DateTimeProperty);
+            set
+            {
+                SetValue(DateTimeProperty, value);
+                OnPropertyChanged("DateTime");
+            }
+        }
+
+        private TimeSpan _time
+        {
+            get => TimeSpan.FromTicks(DateTime.Ticks);
+            set => DateTime = new DateTime(DateTime.Date.Ticks).AddTicks(value.Ticks);
+        }
+
+        private DateTime _date
+        {
+            get => DateTime.Date;
+            set => DateTime = new DateTime(DateTime.TimeOfDay.Ticks).AddTicks(value.Ticks);
+        }
+
+        BindableProperty DateTimeProperty = 
+            BindableProperty.Create(
+                "DateTime", 
+                typeof(DateTime), 
+                typeof(CustomDateTimePicker), 
+                DateTime.Now, 
+                BindingMode.TwoWay, 
+                propertyChanged: DTPropertyChanged
+            );        
+
+        public CustomDateTimePicker()
+        {
+            BindingContext = this;
+#pragma warning disable CS0612 // O tipo ou membro é obsoleto
+            _datePicker.SetBinding<CustomDateTimePicker>(DatePicker.DateProperty, p => p._date);
+#pragma warning restore CS0612 // O tipo ou membro é obsoleto
+
+#pragma warning disable CS0612 // O tipo ou membro é obsoleto
+            _timePicker.SetBinding<CustomDateTimePicker>(TimePicker.TimeProperty, p => p._time);
+#pragma warning restore CS0612 // O tipo ou membro é obsoleto
+
+            _timePicker.Unfocused += (sender, args) => _time = _timePicker.Time;
+            _datePicker.Focused += (s, a) => UpdateEntryText();
+
+            GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() => _datePicker.Focus())
+            });
+            Focused += (sender, args) =>
+            {
+                Device.BeginInvokeOnMainThread(() => _datePicker.Focus());
+            };
+            _datePicker.Unfocused += (sender, args) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    _timePicker.Focus();
+                    _date = _datePicker.Date;
+                    UpdateEntryText();
+                });
+            };
+        }
+
+        private void UpdateEntryText()
+        {
+            Text = DateTime.ToString(StringFormat);
+        }
+
+        static void DTPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var timePicker = (bindable as CustomDateTimePicker);
+            timePicker.UpdateEntryText();
+        }
+    }
+}
--- Prodfy/Prodfy/Views/CadastroAtividadeView.xaml.cs
+++ Prodfy/Prodfy/Views/CadastroAtividadeView.xaml.cs
@@ -13,7 +13,7 @@ namespace Prodfy.Views
 
             NavigationPage.SetHasBackButton(this, false);
 
-            BindingContext = new CadastroAtividadeViewModel();
+            BindingContext = new CadastroAtividadeViewModel();           
         }
     }
 }
