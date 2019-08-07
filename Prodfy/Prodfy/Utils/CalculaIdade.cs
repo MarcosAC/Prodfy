d14edc26c147768510e80ac4116567eff8ee766e
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-08-07 11:02:45 -0300

    Calcula Idade

---
+++ Prodfy/Prodfy/Utils/CalculaIdade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Prodfy.Utils
+{
+    public class CalculaIdade
+    {
+        public static string CalcularPorDataIniciaDataFinal(DateTime dataInicial, DateTime dataFinal)
+        {
+            var idade = dataFinal - dataInicial;
+            return Convert.ToString(idade.Days);
+        }
+    }
+}
