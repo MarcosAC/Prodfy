40093fa9ffbf610a440175fc19a5ac87af860657
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-08-19 16:18:04 -0300

    Estoque Viveiro

--- Prodfy/Prodfy/ViewModels/FuncoesViewModel.cs
+++ Prodfy/Prodfy/ViewModels/FuncoesViewModel.cs
@@ -159,120 +159,84 @@ namespace Prodfy.ViewModels
             _irPaginaIdentificacaoCommand ?? (_irPaginaIdentificacaoCommand = new Command(async () => 
                 await ExecuteIrPaginaIdentificacaoCommand(), CanExecuteFuncaoIdentificacaoCommand));
 
-        private async Task ExecuteIrPaginaIdentificacaoCommand()
-        {
-            await _navigationService.PushAsync(new IdentificacaoView());
-        }
+        private async Task ExecuteIrPaginaIdentificacaoCommand() => await _navigationService.PushAsync(new IdentificacaoView());
 
         private Command _irPaginaEstoqueViveiroCommand;
         public Command IrPaginaEstoqueViveiroCommand =>
             _irPaginaEstoqueViveiroCommand ?? (_irPaginaEstoqueViveiroCommand = new Command(async () =>
                 await ExecuteIrPaginaEstoqueViveiroCommand(), CanExecuteFuncaoEstoqueViveiroCommand));
 
-        private async Task ExecuteIrPaginaEstoqueViveiroCommand()
-        {
-            await _navigationService.PopAsync();
-        }
+        private async Task ExecuteIrPaginaEstoqueViveiroCommand() => await _navigationService.PushAsync(new EstoqueViveiroView());
 
         private Command _irPaginaAtividadeCommand;
         public Command IrPaginaAtividadeCommand =>
             _irPaginaAtividadeCommand ?? (_irPaginaAtividadeCommand = new Command(async () => 
                 await ExecuteIrPaginaAtividadeCommand(), CanExecuteFuncaoAtividadeCommand));
 
-        private async Task ExecuteIrPaginaAtividadeCommand()
-        {
-            await _navigationService.PushAsync(new AtividadeRealizadaView());
-        }
+        private async Task ExecuteIrPaginaAtividadeCommand() => await _navigationService.PushAsync(new AtividadeRealizadaView());
 
         private Command _irPaginaInventarioCommand;
         public Command IrPaginaInventarioCommand =>
             _irPaginaInventarioCommand ?? (_irPaginaInventarioCommand = new Command(async () => 
                 await ExecuteIrPaginaInventarioCommand(), CanExecuteFuncaoInventarioCommand));
 
-        private async Task ExecuteIrPaginaInventarioCommand()
-        {
-            await _navigationService.PushAsync(new CadastroInventarioView());
-        }
+        private async Task ExecuteIrPaginaInventarioCommand() => await _navigationService.PushAsync(new CadastroInventarioView());
 
         private Command _irPaginaMovimentacaoCommand;
         public Command IrPaginaMovimentacaoCommand =>
             _irPaginaMovimentacaoCommand ?? (_irPaginaMovimentacaoCommand = new Command(async () =>
                 await ExecuteIrPaginaMovimentacaoCommand(), CanExecuteFuncaoMovimentacaoCommand));
 
-        private async Task ExecuteIrPaginaMovimentacaoCommand()
-        {
-            await _navigationService.PopAsync();
-        }
+        private async Task ExecuteIrPaginaMovimentacaoCommand() => await _navigationService.PopAsync();
 
         private Command _irPaginaPerdasCommand;
         public Command IrPagianaPerdasCommand =>
             _irPaginaPerdasCommand ?? (_irPaginaPerdasCommand = new Command(async () => 
                 await ExecuteIrPaginaPerdasComand(), CanExecuteFuncaoPerdasCommand));
 
-        private async Task ExecuteIrPaginaPerdasComand()
-        {
-            await _navigationService.PushAsync(new PerdasView());
-        }
+        private async Task ExecuteIrPaginaPerdasComand() => await _navigationService.PushAsync(new PerdasView());
 
         private Command _irPaginaHistoricoCommand;
         public Command IrPaginaHistoricoCommand =>
             _irPaginaHistoricoCommand ?? (_irPaginaHistoricoCommand = new Command(async () => 
                 await ExecuteIrPaginaHistoricoCommand(), CanExecuteFuncaoHistoricoCommand));
 
-        private async Task ExecuteIrPaginaHistoricoCommand()
-        {
-            await _navigationService.PushAsync(new HistoricoView());
-        }        
+        private async Task ExecuteIrPaginaHistoricoCommand() => await _navigationService.PushAsync(new HistoricoView());
 
         private Command _irPaginaLoteCommand;
         public Command IrPaginaLoteCommand =>
             _irPaginaLoteCommand ?? (_irPaginaLoteCommand = new Command(async () => 
                 await ExecuteIrPaginaLoteCommand(), CanExecuteFuncaoMovimentacaoCommand));
 
-        private async Task ExecuteIrPaginaLoteCommand()
-        {
-            await _navigationService.PushAsync(new CadastroEvolucaoLoteView());
-        }
+        private async Task ExecuteIrPaginaLoteCommand() => await _navigationService.PushAsync(new CadastroEvolucaoLoteView());
 
         private Command _irPaginaExpedicaoCommand;
         public Command IrPaginaExpedicaoCommand =>
             _irPaginaExpedicaoCommand ?? (_irPaginaExpedicaoCommand = new Command(async () => 
                 await ExecuteIrPaginaExpedicaoCommand(), CanExecuteFuncaoExpedicaoCommand));
 
-        private async Task ExecuteIrPaginaExpedicaoCommand()
-        {
-            await _navigationService.PushAsync(new CadastroExpedicaoView());
-        }
+        private async Task ExecuteIrPaginaExpedicaoCommand() => await _navigationService.PushAsync(new CadastroExpedicaoView());
 
         private Command _irPaginaLocalizacaoCommand;
         public Command IrPaginaLocalizacaoCommand =>
             _irPaginaLocalizacaoCommand ?? (_irPaginaLocalizacaoCommand = new Command(async () => 
                 await ExecuteIrPaginaLocalizacaoCommand(), CanExecuteFuncaoLocalizacaoCommand));
 
-        private async Task ExecuteIrPaginaLocalizacaoCommand()
-        {
-            await _navigationService.PushAsync(new LocalizacaoView());
-        }
+        private async Task ExecuteIrPaginaLocalizacaoCommand() => await _navigationService.PushAsync(new LocalizacaoView());
 
         private Command _irPaginaOcorrenciaCommand;
         public Command IrPaginaOcorrenciaCommand =>
             _irPaginaOcorrenciaCommand ?? (_irPaginaOcorrenciaCommand = new Command(async () => 
                 await ExecuteIrPaginaOcorrenciaCommand(), CanExecuteFuncaoOcorrenciaCommand));
 
-        private async Task ExecuteIrPaginaOcorrenciaCommand()
-        {
-            await _navigationService.PushAsync(new CadastroOcorrenciaView());
-        }
+        private async Task ExecuteIrPaginaOcorrenciaCommand() => await _navigationService.PushAsync(new CadastroOcorrenciaView());
 
         private Command _irPaginaMedicaoCommand;
         public Command IrPaginaMedicaoCommand =>
             _irPaginaMedicaoCommand ?? (_irPaginaMedicaoCommand = new Command(async () => 
                 await ExecuteIrPaginaMedicaoCommand(), CanExecuteFuncaoMedicaoCommand));
 
-        private async Task ExecuteIrPaginaMedicaoCommand()
-        {
-            await _navigationService.PushAsync(new CadastroMedicaoView());
-        }
+        private async Task ExecuteIrPaginaMedicaoCommand() => await _navigationService.PushAsync(new CadastroMedicaoView());
 
         private Command _refreshCommand;
         public Command RefreshCommand => _refreshCommand ?? (_refreshCommand = new Command(RefresCommandExecute));
