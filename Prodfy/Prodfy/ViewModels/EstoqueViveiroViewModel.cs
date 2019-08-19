d6f5823279e35ac737b2abf303558308c2fe8bfb
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-08-19 16:18:46 -0300

    Estoque Viveiro

---
+++ Prodfy/Prodfy/ViewModels/EstoqueViveiroViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prodfy.Services.Dialog;
+using Prodfy.Services.Navigation;
+
+namespace Prodfy.ViewModels
+{
+    public class EstoqueViveiroViewModel : BaseViewModel
+    {
+        private readonly INavigationService navigationService;
+        private readonly IDialogService dialogService;
+
+        public EstoqueViveiroViewModel()
+        {
+            Title = "Estoque Viveiro";
+
+            navigationService = new NavigationService();
+            dialogService = new DialogService();
+        }
+    }
+}
---
+++ Prodfy/Prodfy/Views/EstoqueViveiroView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Prodfy.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prodfy.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EstoqueViveiroView : ContentPage
+	{
+		public EstoqueViveiroView ()
+		{
+			InitializeComponent ();
+
+            NavigationPage.SetHasBackButton(this, false);
+
+            BindingContext = new EstoqueViveiroViewModel();
+        }
+	}
+}
