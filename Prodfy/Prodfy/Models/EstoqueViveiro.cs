8ff42375de3a8a2e9f8a7b35dfc9d89228047d30
Marcos Aurelio <marcos.aucorrea@gmail.com>
2019-08-20 09:10:11 -0300

    Model EstoqueViveiro

---
+++ Prodfy/Prodfy/Models/EstoqueViveiro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Prodfy.Models
+{
+    public class EstoqueViveiro
+    {
+        public Lote lote { get; set; }
+        public Muda muda { get; set; }
+        public Qualidade qualidade { get; set; }
+        public Estaq estaq { get; set; }
+        public DateTime dataSelecao { get; set; }
+    }
+}
